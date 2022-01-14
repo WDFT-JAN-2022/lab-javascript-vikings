19e9873a979c09095fa7aae9a9fd20e6adda2ff8
Nicholas Badiola <NicholasBadiola@gmail.com>
2022-01-14 12:54:11 -0500

    added Viking

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,23 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(d) {
+    this.health = this.health - d;
+    if (this.health > 0) {
+      return `${this.name} has received ${d} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
 class Saxon {}
